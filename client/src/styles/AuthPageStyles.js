f3a5b218c1a1ae0f9daf30c3fa5fd4bdcbc14442
Kr-Mx <maks.kryshnev@gmail.com>
2020-05-11 18:09:48 +0300

    feat: add styles to auth page

---
+++ client/src/styles/AuthPageStyles.js
@@ -0,0 +1,15 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import red from '@material-ui/core/colors/red';
+
+export const useStyles = makeStyles((theme) => ({
+    cardWrapper: {
+        height: "100vh",
+        backgroundColor:red["100"]
+    },
+    cardContent: {
+        height: "180px",
+    },
+    cardActions: {
+        justifyContent: "center"
+    },
+}));
